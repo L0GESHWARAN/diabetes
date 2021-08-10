bc6aa56f297935df7d96e5a5355873b5ff3bf9c2
LOGESHWARAN <82304120+L0GESHWARAN@users.noreply.github.com>
2021-08-10 11:09:41 +0530

    Update urls.py

--- diabetes/urls.py
+++ diabetes/urls.py
@@ -18,5 +18,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('diabetes/',include('prediction.urls')),
+    path('',include('prediction.urls')),
 ]
